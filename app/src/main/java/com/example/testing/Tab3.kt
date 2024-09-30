599671db2b7754840b359c7e3dd452001c06ca3f
kanitaSiva <kanita.siva@gmail.com>
2024-09-29 22:34:03 -0400

    Fleshed out tab #3, ToDo.

--- app/src/main/java/com/example/testing/Tab3.kt
+++ app/src/main/java/com/example/testing/Tab3.kt
@@ -1,59 +1,76 @@
 package com.example.testing
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.StrikethroughSpan
+import android.text.style.StyleSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.get
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Tab3.newInstance] factory method to
- * create an instance of this fragment.
  */
 class Tab3 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var taskList: ArrayList<String> = ArrayList()
+    private lateinit var tasksAdapter: ArrayAdapter<String>
+    private var listView: ListView? = null
+    private var btnAddTask: Button? = null
+    private var pltxtNewTask: EditText? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab3, container, false)
+        // Inflate layout
+        val view = inflater.inflate(R.layout.fragment_tab3, container, false)
+
+        // Initialize/set up vars
+        listView = view.findViewById(R.id.lstviewTasks)
+        btnAddTask = view.findViewById(R.id.btnAddTask)
+        pltxtNewTask = view.findViewById(R.id.pltxtInputTask)
+        tasksAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, taskList)
+        listView?.adapter = tasksAdapter
+
+        // Set listeners
+        setListViewListener()
+        btnAddTask?.setOnClickListener(::onAddBtnClicked)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Tab3.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Tab3().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun onAddBtnClicked(view: View) {
+        val sItemText: String = pltxtNewTask?.text.toString()
+
+        // Check if task entered is not blank
+        if (sItemText.isNotEmpty()) {
+            tasksAdapter.add(sItemText)
+            tasksAdapter.notifyDataSetChanged()
+            pltxtNewTask?.setText("")
+        } else {
+            Toast.makeText(requireContext(), "An empty task cannot be added", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun setListViewListener() {
+        // Check which item was clicked
+        listView?.setOnItemLongClickListener { _, _, position, _ ->
+            Toast.makeText(requireContext(), "Task removed", Toast.LENGTH_SHORT).show()
+
+            // Remove item at the clicked position
+            taskList.removeAt(position)
+            tasksAdapter.notifyDataSetChanged()
+            true
+        }
     }
 }
